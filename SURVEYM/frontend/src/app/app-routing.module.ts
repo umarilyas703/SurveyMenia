4196a1bf4aaa85cbf2dbb0440a92f3da8c06995f
Umar <umarilyas703@gmail.com>
2023-12-04 01:13:51 +0200

    Minor Change in redirection.

--- SURVEYM/frontend/src/app/app-routing.module.ts
+++ SURVEYM/frontend/src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ import { LanguageFormComponent } from './component/language-form/language-form.c
 //Author: "Traversy Media"
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomepageComponent},
   { path: 'register', component: RegisterComponent},
