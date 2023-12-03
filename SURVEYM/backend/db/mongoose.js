e1e71f4cd9f7acf0d3f333f0da970c01978271f5
Umar <umarilyas703@gmail.com>
2023-12-04 00:51:40 +0200

    Update mongoose.js

--- SURVEYM/backend/db/mongoose.js
+++ SURVEYM/backend/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.set("strictQuery", false);
-mongoose.connect('mongodb+srv://umarilyas703:<Bareer@0315>@cluster0.7dyxvil.mongodb.net/'
+mongoose.connect('mongodb+srv://SurveyMenia:Survey123@cluster0.ccjawzg.mongodb.net/'
 , { useNewUrlParser: true, useUnifiedTopology:true })
     .then(()=>console.log("Database has been connected")) 
     .catch((err)=>console.log(err));
