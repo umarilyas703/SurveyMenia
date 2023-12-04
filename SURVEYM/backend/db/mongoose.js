09ac16aa79141af4fd23c3b540470af07c42a658
Umar <umarilyas703@gmail.com>
2023-12-04 15:37:45 +0200

    change db

--- SURVEYM/backend/db/mongoose.js
+++ SURVEYM/backend/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.set("strictQuery", false);
-mongoose.connect('mongodb+srv://SurveyMenia:Survey123@cluster0.ccjawzg.mongodb.net/'
+mongoose.connect('mongodb+srv://Surveym:surveym@cluster0.fyupojj.mongodb.net/'
 , { useNewUrlParser: true, useUnifiedTopology:true })
     .then(()=>console.log("Database has been connected")) 
     .catch((err)=>console.log(err));
