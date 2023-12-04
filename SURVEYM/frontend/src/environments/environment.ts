8f37aca053e62685f79cd7a6f992375a32d89e3e
Umar <umarilyas703@gmail.com>
2023-12-04 14:07:12 +0200

    CHange in prod

--- SURVEYM/frontend/src/environments/environment.ts
+++ SURVEYM/frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   baseURL: 'https://survey-menia-backend2.onrender.com'
 };
 
